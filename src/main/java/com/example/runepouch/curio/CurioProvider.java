af83223eb04c15aad8d884b389a7a57b48531308
peterbc231 <3638897551@qq.com>
2026-07-25 22:03:15 +0800

    Fix Curios integration by attaching capability dynamically (like CurioOfUndying)

---
+++ src/main/java/com/example/runepouch/curio/CurioProvider.java
@@ -0,0 +1,46 @@
+package com.example.runepouch.curio;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.LazyOptional;
+import top.theillusivec4.curios.api.CuriosCapability;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurio;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class CurioProvider implements ICapabilityProvider {
+    private final LazyOptional<ICurio> curioOptional;
+
+    public CurioProvider(ItemStack stack) {
+        this.curioOptional = LazyOptional.of(() -> new ICurio() {
+            @Override
+            public boolean canEquip(SlotContext slotContext, ItemStack stack) {
+                // 只允许放入 "back" 槽位
+                return slotContext.getIdentifier().equals("back");
+            }
+
+            @Override
+            public boolean canUnequip(SlotContext slotContext, ItemStack stack) {
+                return true;
+            }
+
+            @Override
+            public boolean canEquipFromUse(SlotContext slotContext, ItemStack stack) {
+                return true;
+            }
+        });
+    }
+
+    @Nonnull
+    @Override
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+        if (cap == CuriosCapability.ITEM) {
+            return curioOptional.cast();
+        }
+        return LazyOptional.empty();
+    }
+}
--- src/main/java/com/example/runepouch/item/RunePouchItem.java
+++ src/main/java/com/example/runepouch/item/RunePouchItem.java
@@ -1,16 +1,18 @@
 package com.example.runepouch.item;
 
+import com.example.runepouch.curio.CurioProvider;
 import com.example.runepouch.inventory.RunePouchInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import top.theillusivec4.curios.api.SlotContext;
-import top.theillusivec4.curios.api.type.capability.ICurioItem;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-public class RunePouchItem extends Item implements ICurioItem {
+public class RunePouchItem extends Item {
     public RunePouchItem() {
         super(new Properties().maxStackSize(1).maxDamage(500));
     }
@@ -22,12 +24,27 @@ public class RunePouchItem extends Item implements ICurioItem {
     @Nullable
     @Override
     public ICapabilityProvider initCapabilities(ItemStack stack, @Nullable CompoundNBT nbt) {
-        return new RunePouchInventory(stack);
+        // 返回一个组合的 Capability 提供者
+        return new ICapabilityProvider() {
+            private final RunePouchInventory inventory = new RunePouchInventory(stack);
+            private final CurioProvider curio = new CurioProvider(stack);
+
+            @Nonnull
+            @Override
+            public <T> LazyOptional<T> getCapability(@Nonnull net.minecraftforge.common.capabilities.Capability<T> cap, @Nullable net.minecraft.util.Direction side) {
+                // 优先检查是否是物品处理器能力
+                if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+                    return inventory.getCapability(cap, side);
+                }
+                // 否则检查是否是饰品能力
+                return curio.getCapability(cap, side);
+            }
+        };
     }
 
     @Override
     public CompoundNBT getShareTag(ItemStack stack) {
-        stack.getCapability(net.minecraftforge.items.CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
+        stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
                 .ifPresent(h -> {
                     if (h instanceof RunePouchInventory) {
                         ((RunePouchInventory) h).save();
@@ -40,7 +57,7 @@ public class RunePouchItem extends Item implements ICurioItem {
     public void readShareTag(ItemStack stack, @Nullable CompoundNBT nbt) {
         if (nbt != null) {
             stack.setTag(nbt);
-            stack.getCapability(net.minecraftforge.items.CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
+            stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
                     .ifPresent(h -> {
                         if (h instanceof RunePouchInventory) {
                             ((RunePouchInventory) h).save();
@@ -52,16 +69,6 @@ public class RunePouchItem extends Item implements ICurioItem {
 
     @Override
     public boolean isDamageable(ItemStack stack) {
-        return true;
-    }
-
-    // ========== Curios API 核心方法（无 @Override，避免编译冲突） ==========
-    // 只允许放入 "back" 槽位
-    public boolean canEquip(SlotContext slotContext, ItemStack stack) {
-        return slotContext.getIdentifier().equals("back");
-    }
-
-    public boolean canUnequip(SlotContext slotContext, ItemStack stack) {
         return true;
     }
 }
