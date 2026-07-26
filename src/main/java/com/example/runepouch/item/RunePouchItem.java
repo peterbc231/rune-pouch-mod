93e9f21393d4d5d13f5f24f58b97986cc635d620
peterbc231 <3638897551@qq.com>
2026-07-26 09:39:53 +0800

    Fix compile errors: remove @Override and implement ICurio methods

--- src/main/java/com/example/runepouch/item/RunePouchItem.java
+++ src/main/java/com/example/runepouch/item/RunePouchItem.java
@@ -4,7 +4,14 @@ import com.example.runepouch.inventory.RunePouchInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.Direction;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.util.LazyOptional;
@@ -32,31 +39,44 @@ public class RunePouchItem extends Item {
         return new ICapabilityProvider() {
             private final RunePouchInventory inventory = new RunePouchInventory(stack);
             private final LazyOptional<ICurio> curioOptional = LazyOptional.of(() -> new ICurio() {
-                @Override
-                public boolean canEquip(SlotContext slotContext, ItemStack stack) {
-                    // 允许放入任何槽位
+                // 所有方法都不加 @Override，避免编译冲突
+                public void curioTick(String identifier, int index, LivingEntity livingEntity) {
+                    // 可以在这里添加每个 tick 执行的功能（暂不需要）
+                }
+
+                public void playRightClickEquipSound(LivingEntity livingEntity) {
+                    livingEntity.world.playSound(null, livingEntity.getPosX(), livingEntity.getPosY(),
+                            livingEntity.getPosZ(), SoundEvents.ITEM_ARMOR_EQUIP_LEATHER,
+                            SoundCategory.NEUTRAL, 1.0f, 1.0f);
+                }
+
+                public void onEquip(String identifier, int index, LivingEntity livingEntity) {
+                    // 装备时触发
+                }
+
+                public void onUnequip(String identifier, int index, LivingEntity livingEntity) {
+                    // 卸下时触发
+                }
+
+                public DropRule getDropRule(LivingEntity livingEntity) {
+                    return DropRule.DEFAULT;
+                }
+
+                public boolean canRightClickEquip() {
                     return true;
                 }
 
-                @Override
-                public boolean canUnequip(SlotContext slotContext, ItemStack stack) {
-                    return true;
-                }
-
-                @Override
-                public boolean canEquipFromUse(SlotContext slotContext, ItemStack stack) {
-                    return true;
+                public Multimap<Attribute, AttributeModifier> getAttributeModifiers(String identifier) {
+                    return HashMultimap.create();
                 }
             });
 
             @Nonnull
             @Override
             public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
-                // 如果是物品存储能力，返回 inventory
                 if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
                     return inventory.getCapability(cap, side);
                 }
-                // 如果是饰品能力，返回 curioOptional
                 if (cap == CuriosCapability.ITEM) {
                     return curioOptional.cast();
                 }
